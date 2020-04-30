b803856ad782299050e862abb160ff52cc4073a2
LEEJONGHWA <unknown9732@naver.com>
2020-04-30 12:37:22 +0900

    Swagger 2.6.1 적용, uri 수정

--- src/main/java/com/example/wingapi/web/ArtistController.java
+++ src/main/java/com/example/wingapi/web/ArtistController.java
@@ -1,32 +1,37 @@
 package com.example.wingapi.web;
 
-import com.example.wingapi.domain.artist.Artist;
 import com.example.wingapi.service.ArtistService;
 import com.example.wingapi.web.dto.ArtistResponseDto;
 import com.example.wingapi.web.dto.ArtistSaveRequestDto;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Api(tags = {"Artist Controller"})
 @RestController
 @AllArgsConstructor
-@CrossOrigin(origins = { "http://localhost:3000" })
+@CrossOrigin
 public class ArtistController {
 
     private final ArtistService artistService;
 
-    @GetMapping("/api/get/artist/{id}")
+    @ApiOperation(value = "아티스트 조회", notes = "기본 키 (artist_id)로 조회")
+    @GetMapping("/api/artist/{id}")
     private ArtistResponseDto findById(@PathVariable("id") Long id) {
         return artistService.findById(id);
     }
 
-    @GetMapping("/api/get/artistFindByName/{name}")
-    private List<ArtistResponseDto> findByName(@PathVariable("name") String name) {
+    @ApiOperation(value = "아티스트 검색", notes = "이름으로 검색 후 JSON 리스트로 반환. name 파라미터 필요.")
+    @GetMapping("/api/artist")
+    private List<ArtistResponseDto> findByName(@RequestParam("name") String name) {
         return artistService.findByNameContaining(name);
     }
 
-    @PostMapping("/api/post/artist/")
+    @ApiOperation(value = "아티스트 등록", notes = "아티스트 등록")
+    @PostMapping("/api/artist")
     private Long save(@RequestBody ArtistSaveRequestDto requestDto) {
         return artistService.save(requestDto);
     }
