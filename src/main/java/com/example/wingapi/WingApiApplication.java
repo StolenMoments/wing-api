82e5b9d9a0b594acd59a616d758d78fb11bf5f15
LEEJONGHWA <unknown9732@naver.com>
2020-05-08 22:37:25 +0900

    CORS - 2

--- src/main/java/com/example/wingapi/WingApiApplication.java
+++ src/main/java/com/example/wingapi/WingApiApplication.java
@@ -3,9 +3,11 @@ package com.example.wingapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@CrossOrigin
 public class WingApiApplication {
 
     public static void main(String[] args) {
