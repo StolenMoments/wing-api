8d72346ef800a44b9b945c78545dc6daf44cdc29
LEEJONGHWA <unknown9732@naver.com>
2020-05-08 23:42:30 +0900

    Allow header 수정 ...

--- src/main/java/com/example/wingapi/config/CORSFilter.java
+++ src/main/java/com/example/wingapi/config/CORSFilter.java
@@ -12,9 +12,9 @@ public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4000");
+        response.setHeader("Access-Control-Allow-Origin", "*");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
+        response.setHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
