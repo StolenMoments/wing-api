442a4241c8de56c1b6d775e8fb74c311a57703f7
LEEJONGHWA <unknown9732@naver.com>
2020-05-08 23:35:57 +0900

    CORSFilter...

--- src/main/java/com/example/wingapi/config/CORSFilter.java
+++ src/main/java/com/example/wingapi/config/CORSFilter.java
@@ -12,10 +12,9 @@ public class CORSFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "content-type");
+        response.setHeader("Access-Control-Allow-Origin", "http://localhost:4000");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type");
         filterChain.doFilter(servletRequest, servletResponse);
     }
 
